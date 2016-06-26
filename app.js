a356b2eb403c8064289eb9f6b3fcb48e493dee2a
JiashuLIu <shshshjs1234@gmail.com>
2016-06-26 05:20:28 +0200

    result parse finished

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/:search', function(req, res,next){
     var searchStr = req.params.search;
+    var pagination = req.query.offset || 10;
     var params = {
     "q": searchStr,
     "count": "10",
-    "offset": "0",
+    "offset": pagination,
     "mkt": "en-us",
     "safeSearch": "Moderate",
 };
@@ -19,14 +20,24 @@ app.get('/:search', function(req, res,next){
     var options = {
       url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?'+result,
       headers: {
-        "Ocp-Apim-Subscription-Key": '858a1b03660f4557a6e287f9580e9df6'
+        "Ocp-Apim-Subscription-Key": process.env.API_KEY
       },
     };
 
     function callback(error, response, body) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
-        res.send(info.value);
+        var group=info.value;
+        var endResult=[];
+        group.forEach(function(item) {
+               var set={};
+               set["url"] = item["contentUrl"];
+               set["snippet"] = item["name"];
+               set["thumbnail"] = item["thumbnailUrl"];
+               set["context"] ="https://"+item["hostPageDisplayUrl"];
+               endResult.push(set);
+        });
+        res.send(endResult);
       }
       console.log(response.statusCode);
     };
