ff9d1b39b553f4f2dbb7f39305e0b5118dad7dd7
JiashuLIu <shshshjs1234@gmail.com>
2016-06-26 09:02:11 +0200

    ready to go

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ var app = express();
 var querystring = require("querystring");
 var request=require('request');
 var path = require('path');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/searchlogs');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected");
+});
+var logSchema = new mongoose.Schema({
+term : { type: String },
+date: {type: Date, default: Date.now}
+});
+var searchlog = mongoose.model('searchlog', logSchema);
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/:search', function(req, res,next){
     var searchStr = req.params.search;
     var pagination = req.query.offset || 10;
+    if(searchStr!=="favicon.ico"){
+      var newlog = new searchlog({term:searchStr,date:new Date().toLocaleString()});
+      newlog.save(function (err) {
+        if (err) console.log(err);
+      })
+    };
     var params = {
     "q": searchStr,
     "count": "10",
@@ -43,6 +61,13 @@ app.get('/:search', function(req, res,next){
     };
 
     request(options, callback);
+});
+app.get("/api/recent",function(req,res){
+  searchlog.find({},{ '_id': 0, '__v':0}).sort('-date').limit(10).exec(function(err, posts){
+    console.log("Emitting Update...");
+    console.log("Update Emmited");
+    res.send(posts);
+});
 });
 
 app.listen(app.get('port'),function(){
